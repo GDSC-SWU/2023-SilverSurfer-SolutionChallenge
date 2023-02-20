c1cb8616e7047329984fe8f9b34d13ea76ab6b76
nusuy <nsuy.ch@gmail.com>
2023-02-21 00:19:58 +0900

    chore: Add mysql2 (#1)

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,13 @@
-import mysql from "mysql";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
 // connect db
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DATABASE_SPRINT_HOST,
   user: process.env.DATABASE_SPRINT_USER,
   password: process.env.DATABASE_SPRINT_PASSWORD,
   database: process.env.DATABASE_NAME,
 });
-
-connection.connect();
 
 export default connection;
--- server/routes/Content.js
+++ server/routes/Content.js
@@ -2,14 +2,23 @@ import express from "express";
 import db from "../config/db.js";
 
 const router = express.Router();
+let conn = null;
 
-// 콘텐츠 목록 조회
+// 콘텐츠 목록 조회 (이미지, 타이틀, 설명)
 router.get("/", async (req, res) => {
   try {
-    res.send("hello");
+    conn = await db.getConnection();
+    const [rows] = await conn.query(
+      "select postId, title, explanation from Contents;"
+    );
+
+    //console.log(rows[0].postId);
+    res.send(rows);
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: "Server Error." });
+  } finally {
+    conn.release();
   }
 });
 
