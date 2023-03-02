8a3392a635c1a85d06741571be7a2365eb1eba3e
yeonsu <drawingbonny@gmail.com>
2023-03-02 23:59:43 +0900

    feat: Add route to HomePage (#3)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { GoogleLogin } from "./login/GoogleLogin";
 import { Container } from "./GlobalStyle";
+import { HomePage } from "./main/HomePage";
 
 function App() {
   return (
     <Container>
       <Routes>
         <Route path="/login" element={<GoogleLogin />} />
+        <Route path="/home/*" element={<HomePage />} />
       </Routes>
     </Container>
   );
