c572a9e969a6001579097410048ea2a7b9c50f74
yeonsu <drawingbonny@gmail.com>
2023-03-14 22:22:29 +0900

    feat: Add feedback form (#3)

--- client/src/main/HomePage.jsx
+++ client/src/main/HomePage.jsx
@@ -3,6 +3,7 @@ import Card from "../components/home/Card";
 import NavigationBar from "../components/navigation/NavigationBar";
 import styled from "styled-components";
 import bannerImage from "../assets/main_banner.jpg";
+import InputBox from "../components/home/InputBox";
 
 export const HomePage = () => {
   return (
@@ -13,10 +14,22 @@ export const HomePage = () => {
       </BannerWrapper>
       <Wrapper>
         <GuidelineWrapper>
+          <MainTitle>핵심 지침</MainTitle>
+          <SubTitle>
+            장노년층 사용자를 고려하기 위한 3가지 핵심 가이드라인
+          </SubTitle>
           <Guideline />
           <Guideline />
           <Guideline />
         </GuidelineWrapper>
+      </Wrapper>
+      <Wrapper>
+        <TitleWrapper>
+          <MainTitle>UX 가이드</MainTitle>
+          <SubTitle>
+            장노년층 사용자를 고려하기 위한 5가지 UX 가이드라인
+          </SubTitle>
+        </TitleWrapper>
       </Wrapper>
       <CardWrapper>
         <Card />
@@ -24,6 +37,45 @@ export const HomePage = () => {
         <Card />
         <Card />
       </CardWrapper>
+      <Wrapper>
+        <TitleWrapper>
+          <MainTitle>컴포넌트</MainTitle>
+          <SubTitle>장노년층 사용자를 고려하기 위한 UI 가이드라인</SubTitle>
+        </TitleWrapper>
+      </Wrapper>
+      <CardWrapper>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardWrapper>
+      <Wrapper>
+        <TitleWrapper>
+          <MainTitle>스타일</MainTitle>
+          <SubTitle>장노년층 사용자를 고려하기 위한 UI 가이드라인</SubTitle>
+        </TitleWrapper>
+      </Wrapper>
+      <CardWrapper>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardWrapper>
+      <Wrapper>
+        <TitleWrapper>
+          <MainTitle>의견 제출</MainTitle>
+          <SubTitle>
+            사용해보시고, 개선점이 있을 경우 해당 폼을 작성해주세요. <br /> 더욱
+            좋은 접근성을 제공하는데 있어 큰 도움이 됩니다.{" "}
+          </SubTitle>
+        </TitleWrapper>
+      </Wrapper>
+      <Wrapper>
+        <form action="" method="post">
+          <InputBox />
+          <PostFormButton>보내기</PostFormButton>
+        </form>
+      </Wrapper>
     </div>
   );
 };
@@ -32,6 +84,14 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 73.25rem;
 `;
 
 const GuidelineWrapper = styled.div`
@@ -57,4 +117,31 @@ const MainBannerImage = styled.img`
   display: block;
   margin-left: 7.25rem;
   padding: 1rem;
+`;
+
+const MainTitle = styled.h2`
+  color: ${(props) => props.theme.colors.text_gray1};
+  font-size: 3.125rem;
+  font-weight: 400;
+  margin: 10rem 0 0 0;
+`;
+
+const SubTitle = styled.h4`
+  color: ${(props) => props.theme.colors.text_gray3};
+  font-size: 1.375rem;
+  margin: 0.75rem 0 0 0;
+  display: block;
+  font-weight: 400;
+`;
+
+const PostFormButton = styled.button`
+  width: 72.7rem;
+  height: 4rem;
+  background-color: #d0d0d0;
+  border: none;
+  border-radius: 0.5rem;
+  font-size: 1.5rem;
+  color: #fff;
+  margin-top: 1rem;
+  margin-bottom: 14rem;
 `;
