604909d74f4c7655922d5227b2361dddd20f4cd1
nusuy <nsuy.ch@gmail.com>
2023-03-23 19:01:28 +0900

    feat: Modify Content API (#28)
    - Content list api with token validation middleware
    - Combine bookmark deactivation api with activation api

--- server/routes/Content.js
+++ server/routes/Content.js
@@ -46,6 +46,61 @@ router.get("/:category", async (req, res) => {
   }
 });
 
+// 콘텐츠 목록 조회 (로그인한 경우)
+router.get("/us/:category", validateAccessToken, async (req, res) => {
+  conn = await db.getConnection();
+  try {
+    const userId = req.user;
+    const category = req.params.category;
+    query = `select postId, category, title, title_eng, explanation from Contents where category = "${category}" order by category, postId;`;
+    const [rows] = await conn.query(query);
+
+    // 컨텐츠가 존재할 경우
+    if (rows.length !== 0) {
+      // 해당 회원의 북마크 내역 조회
+      query = `select postId from Scrap where userId = ${userId} order by postId`;
+      const [bookmarks] = await conn.query(query);
+
+      // 썸네일 이미지 조회
+      for (let i = 0; i < rows.length; i++) {
+        query = `select imagePath from Contents_Image where postId = ${rows[i].postId} order by paragraphId asc limit 1`;
+        const [imageRows] = await conn.query(query);
+
+        if (imageRows[0]) {
+          // 컨텐츠 내 이미지 존재 시
+          rows[i].thumbnailPath = imageRows[0].imagePath;
+        } else {
+          // 컨텐츠 내 이미지 미존재 시
+          rows[i].thumbnailPath = null;
+        }
+
+        // 북마크 여부
+        if (bookmarks.length !== 0) {
+          bookmarks.map(
+            (item) => (rows[i].bookmark = item.postId === rows[i].postId)
+          );
+        } else {
+          rows[i].bookmark = false;
+        }
+      }
+    }
+
+    console.log("Successfully Completed.");
+    res.status(200).json({
+      status: "Success",
+      data: rows,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: "Error",
+      message: "Server Error.",
+    });
+  } finally {
+    conn.release();
+  }
+});
+
 // 콘텐츠 세부 조회
 router.get("/detail/:postId", async (req, res) => {
   try {
@@ -112,13 +167,12 @@ router.get("/detail/:postId", async (req, res) => {
   }
 });
 
-// 스크랩 추가
+// 스크랩 추가 및 취소
 router.post("/scrap/:postId", validateAccessToken, async (req, res) => {
+  conn = await db.getConnection();
   try {
     const userId = req.user;
     const postId = Number(req.params.postId);
-
-    conn = await db.getConnection();
 
     // postId 유효성 검사
     query = `select * from Contents where postId = ${postId}`;
@@ -132,28 +186,42 @@ router.post("/scrap/:postId", validateAccessToken, async (req, res) => {
     query = `select * from Scrap where userId = ${userId} and postId = ${postId}`;
     [rows] = await conn.query(query);
 
-    if (rows[0] !== undefined) {
-      throw new Error("Already Saved Data");
+    const isActive = rows[0] !== undefined;
+
+    // Scrap 해제
+    if (isActive) {
+      // Scrap 데이터 삭제
+      query = `delete from Scrap where userId = ${userId} and postId = ${postId}`;
+      await conn.query(query);
+
+      // Users 데이터 내 user의 scrapCount--
+      query = `update Users set scrapCount = scrapCount - 1 where userId = ${userId}`;
+      await conn.query(query);
+
+      // Contents 데이터 내 content의 scrapCount--
+      query = `update Contents set scrapCount = scrapCount - 1 where postId = ${postId}`;
+      await conn.query(query);
+    } else {
+      // Scrap 데이터에 저장
+      query = `insert into Scrap(userId, postId) values("${userId}", "${postId}")`;
+      await conn.query(query);
+
+      // Users 데이터 내 user의 scrapCount++
+      query = `update Users set scrapCount = scrapCount + 1 where userId = ${userId}`;
+      await conn.query(query);
+
+      // Contents 데이터 내 content의 scrapCount++
+      query = `update Contents set scrapCount = scrapCount + 1 where postId = ${postId}`;
+      await conn.query(query);
     }
-
-    // Scrap 데이터에 저장
-    query = `insert into Scrap(userId, postId) values("${userId}", "${postId}")`;
-    await conn.query(query);
-
-    // Users 데이터 내 user의 scrapCount++
-    query = `update Users set scrapCount = scrapCount + 1 where userId = ${userId}`;
-    await conn.query(query);
-
-    // Contents 데이터 내 content의 scrapCount++
-    query = `update Contents set scrapCount = scrapCount + 1 where postId = ${postId}`;
-    await conn.query(query);
 
     console.log("Updated Successfully.");
 
     // 응답 전달
     res.status(200).send({
       status: "Success",
-      message: "Saved Successfully.",
+      message: "Updated Successfully.",
+      isActive: !isActive,
     });
   } catch (err) {
     console.error(err);
@@ -161,10 +229,7 @@ router.post("/scrap/:postId", validateAccessToken, async (req, res) => {
     let errCode = 500;
     let errMessage = "Server Error";
 
-    if (
-      err.message === "Invalid PostId" ||
-      err.message === "Already Saved Data"
-    ) {
+    if (err.message === "Invalid PostId") {
       errCode = 404;
       errMessage = err.message;
     }
@@ -175,69 +240,6 @@ router.post("/scrap/:postId", validateAccessToken, async (req, res) => {
     });
   } finally {
     // db 연결 종료
-    conn.release();
-  }
-});
-
-// 스크랩 취소
-router.patch("/scrap/:postId", validateAccessToken, async (req, res) => {
-  try {
-    const userId = req.user;
-    const postId = Number(req.params.postId);
-
-    conn = await db.getConnection();
-
-    // postId 유효성 검사
-    query = `select * from Contents where postId = ${postId}`;
-    let [rows] = await conn.query(query);
-
-    if (!rows[0]) {
-      throw new Error("Invalid PostId");
-    }
-
-    // Scrap 데이터 검사
-    query = `select * from Scrap where userId = ${userId} and postId = ${postId}`;
-    [rows] = await conn.query(query);
-
-    if (!rows[0]) {
-      throw new Error("Invalid Data");
-    }
-
-    // Scrap 데이터 삭제
-    query = `delete from Scrap where userId = ${userId} and postId = ${postId}`;
-    await conn.query(query);
-
-    // Users 데이터 내 user의 scrapCount--
-    query = `update Users set scrapCount = scrapCount - 1 where userId = ${userId}`;
-    await conn.query(query);
-
-    // Contents 데이터 내 content의 scrapCount--
-    query = `update Contents set scrapCount = scrapCount - 1 where postId = ${postId}`;
-    await conn.query(query);
-
-    console.log("Updated Successfully.");
-
-    // 응답 전달
-    res.status(200).send({
-      status: "Success",
-      message: "Updated Successfully.",
-    });
-  } catch (err) {
-    console.error(err);
-
-    let errCode = 500;
-    let errMessage = "Server Error";
-
-    if (err.message === "Invalid PostId" || err.message === "Invalid Data") {
-      errCode = 404;
-      errMessage = err.message;
-    }
-
-    res.status(errCode).send({
-      status: "Error",
-      message: errMessage,
-    });
-  } finally {
     conn.release();
   }
 });
